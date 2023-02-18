b2142dfc3edaa47deb3ccbb40e5c870e48c856b7
Wivik <48727868+Wivik@users.noreply.github.com>
2023-02-18 18:51:54 +0100

    feat: :sparkles: init repository

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask
+from flask import render_template, request
+import psutil
+from geopy.geocoders import Nominatim
+import requests
+
+
+app = Flask(__name__)
+geolocator = Nominatim(user_agent="pyp-boy")
+# url = 'http://ipinfo.io/json'
+# response = requests.get(url)
+# print(response.json())
+# print(response.json()['ip'])
+
+
+## def general vars for footer
+def footer_vars():
+    cpu_percent = psutil.cpu_percent(interval=0.1)
+    footer_vars = {
+        'cpu_percent': cpu_percent,
+    }
+
+    return footer_vars
+
+footer_vars = footer_vars()
+
+@app.route("/")
+@app.route("/stat")
+def stat():
+    return render_template('stat.html', footer_vars=footer_vars)
+
+@app.route("/inv")
+def inventory():
+    return render_template('inv.html', footer_vars=footer_vars)
+
+@app.route("/map", methods=['GET'])
+@app.route("/map/", methods=['GET'])
+def map():
+    url = 'http://ipinfo.io/json'
+    response = requests.get(url)
+    # print(response.json())
+    # print(response.json()['ip'])
+    get_latitude = request.args.get('latitude', '0')
+    get_longitude = request.args.get('longitude', '0')
+
+    if get_latitude == '0' and get_longitude == '0':
+        geoloc_request = response.json()['loc']
+    else:
+        geoloc_request = str(get_latitude) + ',' +str(get_longitude)
+
+    print(geoloc_request)
+    location = geolocator.geocode(geoloc_request)
+    return render_template('map.html', footer_vars=footer_vars, location=location)
+
+@app.route("/map/search", methods=('GET', 'POST'))
+def map_search():
+    if request.method == 'POST':
+        map_search_item = request.form['map_search_item']
+        search_location = geolocator.geocode(map_search_item, exactly_one=False, limit=10)
+        print(search_location)
+        # print(search_location.latitude)
+        # print(search_location.longitude)
+        return render_template('map_search.html', footer_vars=footer_vars, search_results=search_location)
+
+    else:
+        return render_template('map_search.html', footer_vars=footer_vars)
+
+
