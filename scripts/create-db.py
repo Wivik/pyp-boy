6ab4b776dc7016a5963d4ea1cb389078ad048c8e
Wivik <48727868+Wivik@users.noreply.github.com>
2023-03-18 22:30:40 +0100

    feat: database creation updated

--- scripts/create-db.py
+++ scripts/create-db.py
@@ -12,20 +12,19 @@ version = str(version['version'])
 conn = sqlite3.connect('game.db')
 c = conn.cursor()
 
-with open('../sql/game_db_story.sql', 'r') as file:
-    sql = file.read()
-    c.execute(sql)
-    conn.commit()
+game_db_files = [
+    '../sql/game_db_story.sql',
+    '../sql/game_db_weapon.sql',
+    '../sql/game_db_apparel.sql',
+    '../sql/game_db_version.sql',
+]
 
-with open('../sql/game_db_version.sql', 'r') as file:
-    sql = file.read()
-    c.execute(sql)
-    conn.commit()
+for game_db_file in game_db_files:
+    with open(game_db_file, 'r') as file:
+        sql = file.read()
+        c.execute(sql)
+        conn.commit()
 
-with open('../sql/game_db_weapons.sql', 'r') as file:
-    sql = file.read()
-    c.execute(sql)
-    conn.commit()
 
 
 c.execute('INSERT INTO db_version values(?)', (version,))
@@ -38,8 +37,34 @@ with open('../sql/story.csv', 'r') as story_file:
         # print(row)
         row = [None if value.strip() == '' else value.strip() for value in row]
 
-        c.execute('INSERT INTO story VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', row)
+        c.execute('INSERT INTO story VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', row)
 
     conn.commit()
 
     conn.close()
+
+inventory_files = [
+    {'name': 'apparel', 'file': '../sql/apparel.csv'},
+    {'name': 'weapon', 'file': '../sql/weapon.csv'},
+]
+
+
+conn = sqlite3.connect('game.db')
+c = conn.cursor()
+
+for inv_file in inventory_files:
+
+    with open(inv_file['file'], 'r') as load_inv_file:
+        inv = csv.reader(load_inv_file)
+
+        for row in inv:
+            # print(row)
+
+            c.execute('INSERT INTO '+ inv_file['name'] +' VALUES (?, ?, ?, ?, ?)', (row))
+
+conn.commit()
+
+conn.close()
+
+
+
